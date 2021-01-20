c3a206a35138c0bf3a26cf29c37a37dde6da743e
Polikseni Lapa <polikseni.lapa@gov.local>
2021-01-20 14:09:06 +0100

    we

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -4,8 +4,27 @@ import React, { useEffect, useRef } from 'react';
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
+
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const refContainer = useRef (null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+
+  };
+ 
+  return (
+    <>
+    <form className='form' onSubmit={handleSubmit}>
+      <div>
+        <input type='text' ref={refContainer} />
+        <button type='button'>Submit</button>
+      </div>
+    </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
